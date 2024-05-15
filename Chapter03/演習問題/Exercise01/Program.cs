2b0db1e9a416c401772d70843036412d7258a799
UchiyamaNaoto <63093527+UchiyamaNaoto@users.noreply.github.com>
2024-05-15 15:21:46 +0900

    解説済み

--- Chapter03/演習問題/Exercise01/Program.cs
+++ Chapter03/演習問題/Exercise01/Program.cs
@@ -26,19 +26,24 @@ namespace Exercise01 {
         }
 
         private static void Exercise1_1(List<int> numbers) {
-        
+            var exist = numbers.Exists(n => n % 8 == 0 || n % 9 == 0);
+            if (exist)
+                Console.WriteLine("存在しています");
+            else
+                Console.WriteLine("存在していません");
         }
 
         private static void Exercise1_2(List<int> numbers) {
-        
+            numbers.ForEach(n => Console.WriteLine(n / 2.0));
         }
 
         private static void Exercise1_3(List<int> numbers) {
-        
+            numbers.Where(n => 50 <= n).ToList()
+                                        .ForEach(n => Console.WriteLine(n));
         }
 
         private static void Exercise1_4(List<int> numbers) {
-        
+            numbers.Select(n => n * 2).ToList().ForEach(n => Console.WriteLine(n));
         }
     }
 }
