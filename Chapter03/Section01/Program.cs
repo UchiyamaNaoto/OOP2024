19cf3dfccc7afeaa72b1acc6a3663e7edb0961c0
UchiyamaNaoto <63093527+UchiyamaNaoto@users.noreply.github.com>
2024-05-15 11:48:35 +0900

    P87まで

---
+++ Chapter03/Section01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
+
+            //int count = numbers.Count(n => n % 2 == 0);
+            double num = numbers.Where(n => n > 5).Average();
+            int total = numbers.Where(n => n > 5).Sum();
+            Console.WriteLine(num);
+            Console.WriteLine(total);
+
+        }
+
+    }
+}
