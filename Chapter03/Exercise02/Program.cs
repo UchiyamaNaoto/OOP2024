eeef2def768febc6b541b3b2add2e51edac8e3d4
UchiyamaNaoto <63093527+UchiyamaNaoto@users.noreply.github.com>
2024-05-15 15:57:57 +0900

    演習問題3.２準備

---
+++ Chapter03/Exercise02/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            var names = new List<string> {
+                "Tokyo", "New Delhi", "Bangkok", "London",
+                "Paris", "Berlin", "Canberra", "Hong Kong",
+            };
+
+            Exercise2_1(names);
+            Console.WriteLine();
+            Exercise2_2(names);
+            Console.WriteLine();
+            Exercise2_3(names);
+            Console.WriteLine();
+            Exercise2_4(names);
+        }
+
+        private static void Exercise2_1(List<string> names) {
+        
+        }
+
+        private static void Exercise2_2(List<string> names) {
+
+        }
+
+        private static void Exercise2_3(List<string> names) {
+
+        }
+
+        private static void Exercise2_4(List<string> names) {
+
+        }
+    }
+}
--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -18,10 +18,17 @@ namespace Section01 {
                 "Hong Kong",
             };
 
-            IEnumerable<string> query = names.Where(s=>s.Contains(" "))
-                                    .Select(s=>s.ToUpper());
-            foreach (string s in query)
-                Console.WriteLine(s);
+            var query = names.Where(s => s.Length <= 5).ToArray(); 
+            foreach (var item in query) {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("-----------");
+
+            names[0] = "Osaka";
+            foreach (var item in query) {
+                Console.WriteLine(item);
+            }
 
         }
 
