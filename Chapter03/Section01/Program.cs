d51848d9e4f0df78d9ad65b37f567ffe1183e5d1
UchiyamaNaoto <63093527+UchiyamaNaoto@users.noreply.github.com>
2024-05-15 14:16:40 +0900

    演習問題まで

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -7,13 +7,21 @@ using System.Threading.Tasks;
 namespace Section01 {
     internal class Program {
         static void Main(string[] args) {
-            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
+            var names = new List<string> {
+                "Tokyo",
+                "New Delhi",
+                "Bangkok",
+                "London",
+                "Paris",
+                "Berlin",
+                "Canberra",
+                "Hong Kong",
+            };
 
-            //int count = numbers.Count(n => n % 2 == 0);
-            double num = numbers.Where(n => n > 5).Average();
-            int total = numbers.Where(n => n > 5).Sum();
-            Console.WriteLine(num);
-            Console.WriteLine(total);
+            IEnumerable<string> query = names.Where(s=>s.Contains(" "))
+                                    .Select(s=>s.ToUpper());
+            foreach (string s in query)
+                Console.WriteLine(s);
 
         }
 
---
+++ Chapter03/演習問題/Exercise01/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var numbers = new List<int> { 12, 87, 94, 14, 53, 20, 40, 35, 76, 91, 31, 17, 48 };
+
+            // 3.1.1
+            Exercise1_1(numbers);
+            Console.WriteLine("-----");
+
+            // 3.1.2
+            Exercise1_2(numbers);
+            Console.WriteLine("-----");
+
+            // 3.1.3
+            Exercise1_3(numbers);
+            Console.WriteLine("-----");
+
+            // 3.1.4
+            Exercise1_4(numbers);
+        }
+
+        private static void Exercise1_1(List<int> numbers) {
+        
+        }
+
+        private static void Exercise1_2(List<int> numbers) {
+        
+        }
+
+        private static void Exercise1_3(List<int> numbers) {
+        
+        }
+
+        private static void Exercise1_4(List<int> numbers) {
+        
+        }
+    }
+}
