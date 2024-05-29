35b25db906f99a9d46e6fcaea370be72d841dee2
UchiyamaNaoto <63093527+UchiyamaNaoto@users.noreply.github.com>
2024-05-29 15:12:23 +0900

    第4章演習問題終了

---
+++ Chapter04/演習問題/Exercise01/YearMonth.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+
+    public class YearMonth {
+        // 4.1.1
+        public int Year { get; private set; }
+        public int Month { get; private set; }
+
+        //コンストラクタ
+        public YearMonth(int year, int month) {
+            Year = year;
+            Month = month;
+        }
+
+        // 4.1.2
+        public bool Is21Century {
+            get {
+                return 2001 <= Year && Year <= 2100;
+            }
+        }
+
+        // 4.1.3
+        public YearMonth AddOneMonth() {
+            if (Month == 12) {
+                return new YearMonth(Year + 1, 1);
+            } else {
+                return new YearMonth(Year, Month + 1);
+            }
+        }
+
+        // 4.1.4
+        public override string ToString() {
+            //return Year + "年" + Month + "月";
+            return $"{Year}年{Month}月";
+        }
+    }
+}
---
+++ Chapter04/演習問題/Exercise02/Program.cs
@@ -0,0 +1,67 @@
+﻿using Exercise01;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            // 4.2.1
+            var ymCollection = new YearMonth[] {
+                new YearMonth(1980, 1),
+                new YearMonth(1990, 4),
+                new YearMonth(2000, 7),
+                new YearMonth(2010, 9),
+                new YearMonth(2020, 12),
+            };
+
+            // 4.2.2
+            Console.WriteLine("\n- 4.2.2 ---");
+            Exercise2_2(ymCollection);
+            Console.WriteLine("\n- 4.2.4 ---");
+
+            // 4.2.4
+            Exercise2_4(ymCollection);
+            Console.WriteLine("\n- 4.2.5 ---");
+
+
+            // 4.2.5
+            Exercise2_5(ymCollection);
+        }
+
+        private static void Exercise2_2(YearMonth[] ymCollection) {
+            foreach (var ym in ymCollection) {
+                Console.WriteLine(ym);
+            }
+        }
+
+        // 4.2.3(4.2.4で呼び出されるメソッド)
+        static YearMonth FindFirst21C(YearMonth[] yms) {
+            foreach (var ym in yms) {
+                if (ym.Is21Century)
+                    return ym;
+            }
+            return null;
+        }
+
+        private static void Exercise2_4(YearMonth[] ymCollection) {
+            //var ym = FindFirst21C(ymCollection);
+            //if(ym != null) {
+            //    Console.WriteLine(ym);
+            //} else {
+            //    Console.WriteLine("21世紀のデータはありません");
+            //}
+            var ym = FindFirst21C(ymCollection);
+            Console.WriteLine(ym == null ? "21世紀のデータはありません" : ym.ToString());
+        }
+
+        private static void Exercise2_5(YearMonth[] ymCollection) {
+            var array = ymCollection.Select(ym => ym.AddOneMonth()).ToArray();
+            foreach (var ym in array) {
+                Console.WriteLine(ym);
+            }
+        }
+    }
+}
