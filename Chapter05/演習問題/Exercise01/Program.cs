192e5d96f686018ab690bb873024cfabce9bffa6
UchiyamaNaoto <63093527+UchiyamaNaoto@users.noreply.github.com>
2024-05-29 15:51:04 +0900

    第5章演習問題ひな形作成

---
+++ Chapter05/演習問題/Exercise01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var s1 = Console.ReadLine();
+            var s2 = Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ Chapter05/演習問題/Exercise03/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise03 {
+    internal class Program {
+        static void Main(string[] args) {
+            var text = "Jackdaws love my big sphinx of quartz";
+
+            Exercise3_1(text);
+            Console.WriteLine("-----");
+
+            Exercise3_2(text);
+            Console.WriteLine("-----");
+
+            Exercise3_3(text);
+            Console.WriteLine("-----");
+
+            Exercise3_4(text);
+            Console.WriteLine("-----");
+
+            Exercise3_5(text);
+        }
+
+        private static void Exercise3_1(string text) {
+        
+        }
+
+        private static void Exercise3_2(string text) {
+        
+        }
+
+        private static void Exercise3_3(string text) {
+        
+        }
+
+        private static void Exercise3_4(string text) {
+        
+        }
+
+        private static void Exercise3_5(string text) {
+        
+        }
+    }
+}
