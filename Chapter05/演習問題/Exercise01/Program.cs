ea8d97975a1f8c4f1e97685d5a2e1250bd3a4003
UchiyamaNaoto <63093527+UchiyamaNaoto@users.noreply.github.com>
2024-05-30 13:22:41 +0900

    問題5.1完成

--- Chapter05/演習問題/Exercise01/Program.cs
+++ Chapter05/演習問題/Exercise01/Program.cs
@@ -10,8 +10,10 @@ namespace Exercise01 {
             var s1 = Console.ReadLine();
             var s2 = Console.ReadLine();
 
-
-
+            if (string.Compare(s1, s2, ignoreCase:true) == 0)
+                Console.WriteLine("等しい");
+            else
+                Console.WriteLine("等しくない");
         }
     }
 }
