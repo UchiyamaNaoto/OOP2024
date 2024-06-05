2f0fc4a9b4fc889602be14750a9a3fb8a684847c
UchiyamaNaoto <63093527+UchiyamaNaoto@users.noreply.github.com>
2024-06-05 10:23:06 +0900

    第５章演習問題完成

--- Chapter05/演習問題/Exercise02/Program.cs
+++ Chapter05/演習問題/Exercise02/Program.cs
@@ -7,6 +7,14 @@ using System.Threading.Tasks;
 namespace Exercise02 {
     internal class Program {
         static void Main(string[] args) {
+            var line = Console.ReadLine();
+            int num;
+            if (int.TryParse(line, out num)) {
+                Console.WriteLine($"{num:#,#}");
+                //Console.WriteLine("{0:#,#}", num);
+            } else {
+                Console.WriteLine("数字文字列でありません");
+            }
         }
     }
 }
--- Chapter05/演習問題/Exercise03/Program.cs
+++ Chapter05/演習問題/Exercise03/Program.cs
@@ -8,7 +8,9 @@ namespace Exercise03 {
     internal class Program {
         static void Main(string[] args) {
             var text = "Jackdaws love my big sphinx of quartz";
+            var text2 = "Jackdaws,love my,big sphinx-of_quartz";
 
+            #region
             Exercise3_1(text);
             Console.WriteLine("-----");
 
@@ -22,26 +24,53 @@ namespace Exercise03 {
             Console.WriteLine("-----");
 
             Exercise3_5(text);
+            Console.WriteLine("-----");
+
+            Exercise3_6(text2);
+
+            #endregion
         }
 
         private static void Exercise3_1(string text) {
-        
+            var spaces = text.Count(c => c == ' ');
+            Console.WriteLine("空白数:{0}", spaces);
         }
 
         private static void Exercise3_2(string text) {
-        
+            var replaced = text.Replace("big", "small");
+            Console.WriteLine(replaced);
         }
 
         private static void Exercise3_3(string text) {
-        
+            int count = text.Split(' ').Length;
+            Console.WriteLine("単語数:{0}", count);
         }
 
         private static void Exercise3_4(string text) {
-        
+            var words = text.Split(' ').Where(s => s.Length <= 4);
+            foreach (var word in words) {
+                Console.WriteLine(word);
+            }
         }
 
         private static void Exercise3_5(string text) {
-        
+            var array = text.Split(' ').ToArray();
+
+            if (array.Length > 0) {
+                var sb = new StringBuilder(array[0]);
+                foreach (var word in array.Skip(1)) {
+                    sb.Append(' ');
+                    sb.Append(word);
+                }
+                Console.WriteLine(sb);
+            }
+        }
+
+        private static void Exercise3_6(string text) {
+            var array = text.Split(new[] { ' ', ',', '-', '_' }).ToArray();
+            foreach (var word in array) {
+                Console.WriteLine(word);
+            }
         }
     }
 }
