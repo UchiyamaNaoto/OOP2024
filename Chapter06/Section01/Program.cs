bc48abc8cc0df9669ffb0bb28c51d74924e8017f
UchiyamaNaoto <63093527+UchiyamaNaoto@users.noreply.github.com>
2024-06-05 13:26:20 +0900

    第6章演習問題

---
+++ Chapter06/Section01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var books = Books.GetBooks().OrderByDescending(b=>b.Price).ToList();
+
+            books.ForEach(b=>Console.WriteLine(b.Title + ":" + b.Price));
+
+            //foreach (var book in books) {
+            //    Console.WriteLine(book.Title + ":" + book.Price);
+            //}
+            
+        }
+    }
+}
---
+++ Chapter06/演習問題/Exercise01/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var numbers = new int[] { 5, 10, 17, 9, 3, 21, 10, 40, 21, 3, 35 };
+
+            Exercise1_1(numbers);
+            Console.WriteLine("-----");
+
+            Exercise1_2(numbers);
+            Console.WriteLine("-----");
+
+            Exercise1_3(numbers);
+            Console.WriteLine("-----");
+
+            Exercise1_4(numbers);
+            Console.WriteLine("-----");
+
+            Exercise1_5(numbers);
+        }
+
+        private static void Exercise1_1(int[] numbers) {
+        
+        }
+
+        private static void Exercise1_2(int[] numbers) {
+        
+        }
+
+        private static void Exercise1_3(int[] numbers) {
+        
+        }
+
+        private static void Exercise1_4(int[] numbers) {
+        
+        }
+
+        private static void Exercise1_5(int[] numbers) {
+        
+        }
+    }
+}
