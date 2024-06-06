a23089464686eb173faf4508110655a99cb8e4c0
UchiyamaNaoto <63093527+UchiyamaNaoto@users.noreply.github.com>
2024-06-06 13:41:00 +0900

    P185まで終了

--- Chapter06/演習問題/Exercise01/Program.cs
+++ Chapter06/演習問題/Exercise01/Program.cs
@@ -25,23 +25,33 @@ namespace Exercise01 {
         }
 
         private static void Exercise1_1(int[] numbers) {
-        
+            var number = numbers.Max();
+            Console.WriteLine(number);
         }
 
         private static void Exercise1_2(int[] numbers) {
-        
+            var skip = numbers.Length - 2;
+            foreach (var number in numbers.Skip(skip)){
+                Console.WriteLine(number);
+            }
         }
 
         private static void Exercise1_3(int[] numbers) {
-        
+            var strings = numbers.Select(n => n.ToString());
+            foreach (var numStering in strings) {
+                Console.WriteLine(numStering);
+            }
         }
 
         private static void Exercise1_4(int[] numbers) {
-        
+            foreach (var number in numbers.OrderBy(n=>n).Take(3)) {
+                Console.WriteLine(number);
+            }
         }
 
         private static void Exercise1_5(int[] numbers) {
-        
+            var count = numbers.Distinct().Count(n => n > 10);
+            Console.WriteLine(count);   
         }
     }
 }
