95330568b3590784740754246122e28b36e94fdd
UchiyamaNaoto <63093527+UchiyamaNaoto@users.noreply.github.com>
2024-06-18 10:36:44 +0900

    P195上まで

---
+++ Chapter07/Section02/MonthDay.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section02 {
+    internal class MonthDay {
+        public int Month { get; private set; }
+        public int Day { get; private set; }
+
+        public MonthDay(int month, int day)
+        {
+            this.Month = month; 
+            this.Day = day;
+        }
+
+        //MonthDayどうしの比較をする
+        public override bool Equals(object obj) {
+            var other = obj as MonthDay; 
+            if(other == null) {
+                throw new ArgumentException();
+            }
+            return this.Month == other.Month && this.Day == other.Day;
+        }
+
+        //ハッシュコードを求める
+        public override int GetHashCode() { 
+            return Month.GetHashCode() * 31 + Day.GetHashCode();
+        }
+    }
+}
---
+++ Chapter07/Section02/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section02 {
+    internal class Program {
+        static void Main(string[] args) {
+            var lines = File.ReadAllLines("sample.txt");
+            var we = new WordsExtractor(lines);
+            foreach (var word in we.Extract()) {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
