7ff3ebb41f112d1b791cc6f108f02acd8813027e
UchiyamaNaoto <63093527+UchiyamaNaoto@users.noreply.github.com>
2024-07-19 11:15:09 +0900

    P299演習問題完成

---
+++ Chapter11/Section02/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Section02 {
+    internal class Program {
+        static void Main(string[] args) {
+            AddElement();
+        }
+
+        public static void AddElement() {
+            var element = new XElement("novelist",
+                new XElement("name", "菊池 寛", new XAttribute("kana", "きくち かん")),
+                new XElement("birth", "1888-12-26"),
+                new XElement("death", "1948-03-06"),
+                new XElement("masterpieces",
+                  new XElement("title", "恩讐の彼方に"),
+                  new XElement("title", "真珠夫人")
+                )
+              );
+            var xdoc = XDocument.Load("novelists.xml");
+            xdoc.Root.Add(element);
+
+            // これ以降は確認用のコード 
+            foreach (var xnovelist in xdoc.Root.Elements()) {
+                var xname = xnovelist.Element("name");
+                var birth = (DateTime)xnovelist.Element("birth");
+                Console.WriteLine("{0} {1}", xname.Value, birth.ToShortDateString());
+            }
+
+            //保存
+            xdoc.Save("newXmlFile.xml");
+        }
+    }
+}
--- Chapter11/演習問題/Exercise01/Program.cs
+++ Chapter11/演習問題/Exercise01/Program.cs
@@ -3,10 +3,86 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Exercise01 {
     internal class Program {
         static void Main(string[] args) {
+            var file = "sample.xml";
+            Exercise1_1(file);
+            Console.WriteLine();
+            Exercise1_2(file);
+            Console.WriteLine();
+            Exercise1_3(file);
+            Console.WriteLine();
+
+            var newfile = "sports.xml";
+            Exercise1_4(file, newfile);
+
+        }
+
+        private static void Exercise1_1(string file) {
+            var xdoc = XDocument.Load(file);
+            var sports = xdoc.Root.Elements()
+                             .Select(x => new {
+                                 Name = x.Element("name").Value,
+                                 Teammembers = x.Element("teammembers").Value
+                             });
+            foreach (var sport in sports) {
+                Console.WriteLine("{0} {1}", sport.Name, sport.Teammembers);
+            }
+        }
+
+        private static void Exercise1_2(string file) {
+            var xdoc = XDocument.Load(file);
+            var sports = xdoc.Root.Elements()
+                            .Select(x => new {
+                                Name = x.Element("name").Attribute("kanji").Value,
+                                Firstplayed = x.Element("firstplayed").Value,
+                            }).OrderBy(x => x.Firstplayed);
+
+            foreach (var sport in sports) {
+                Console.WriteLine("{0}({1})", sport.Name, sport.Firstplayed);
+            }
+
+        }
+
+        private static void Exercise1_3(string file) {
+#if false 
+            //解答例①
+            var xdoc = XDocument.Load(file);
+            var sport = xdoc.Root.Elements()
+                            .OrderByDescending(x => x.Element("teammembers").Value)
+                            .First();
+
+            Console.WriteLine($"{sport.Element("name").Value}");
+#else 
+            //解答例②
+            var xdoc = XDocument.Load(file);
+            var sport = xdoc.Root.Elements()
+                             .Select(x => new {
+                                 Name = x.Element("name").Value,
+                                 Teammembers = x.Element("teammembers").Value
+                             })
+                             .OrderByDescending(x => int.Parse(x.Teammembers))
+                             .First();
+            Console.WriteLine("{0}", sport.Name);
+#endif
+
+        }
+
+        private static void Exercise1_4(string file, string newfile) {
+            var element = new XElement("ballsport",
+                 new XElement("name", "サッカー", new XAttribute("kanji", "蹴球")),
+                 new XElement("teammembers", "11"),
+                 new XElement("firstplayed", "1863")
+            );
+
+            var xdoc = XDocument.Load(file);
+            xdoc.Root.Add(element);
+
+            //保存
+            xdoc.Save(newfile);
         }
     }
 }
