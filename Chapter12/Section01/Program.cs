51f46c288eda61674a24942b5fcc46fab1c9cf75
UchiyamaNaoto <63093527+UchiyamaNaoto@users.noreply.github.com>
2024-08-27 10:30:19 +0900

    第12章シリアル化・逆シリアル化処理実装P303まで

---
+++ Chapter12/Section01/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+
+#if false
+            var novel = new Novel {
+                Author = "ジェイムズ・P・ホーガン",
+                Title = "星を継ぐもの",
+                Published = 1977,
+            };
+
+            var settings = new XmlWriterSettings {
+                Encoding = new UTF8Encoding(false),
+                Indent = true,
+                IndentChars = " ",
+            };
+
+            using (var writer = XmlWriter.Create("novel.xml", settings)) {
+                var serializer = new DataContractSerializer(novel.GetType());
+                serializer.WriteObject(writer, novel);
+            }
+#else
+            using (var reader = XmlReader.Create("novel.xml")) {
+                var serializer = new DataContractSerializer(typeof(Novel));
+                var novel = serializer.ReadObject(reader) as Novel;
+                Console.WriteLine(novel);
+            }
+#endif
+        }
+    }
+}
