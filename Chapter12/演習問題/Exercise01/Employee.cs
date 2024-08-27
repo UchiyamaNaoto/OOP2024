5a8c65a1391f6cd84f4bc6b69d60cf26e0f08c37
UchiyamaNaoto <63093527+UchiyamaNaoto@users.noreply.github.com>
2024-08-27 12:10:30 +0900

    12.1.2まで完了

---
+++ Chapter12/演習問題/Exercise01/Employee.cs
@@ -0,0 +1,8 @@
+using System;
+namespace Exercise01 {
+    public class Employee {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime HireDate { get; set; }
+    }
+}
---
+++ Chapter12/演習問題/Exercise01/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            Exercise1_1("employee.xml");
+
+            // これは確認用
+            Console.WriteLine(File.ReadAllText("employee.xml"));
+            Console.WriteLine();
+
+            Exercise1_2("employees.xml");
+            Exercise1_3("employees.xml");
+            Console.WriteLine();
+
+            //Exercise1_4("employees.json");
+
+            //// これは確認用
+            //Console.WriteLine(File.ReadAllText("employees.json"));
+        }
+
+        private static void Exercise1_1(string outfile) {
+            var emp = new Employee {
+                Id = 123,
+                Name = "出井 秀行",
+                HireDate = new DateTime(2001, 5, 10),
+            };
+
+            using (var writer = XmlWriter.Create(outfile)) {
+                var serializer = new XmlSerializer(emp.GetType());
+                serializer.Serialize(writer, emp);
+            }
+        }
+
+        private static void Exercise1_2(string outfile) {
+            var emps = new Employee[] {
+                new Employee {
+                    Id = 123,
+                    Name = "出井 秀行",
+                    HireDate = new DateTime(2001, 5, 10),
+                },
+                new Employee {
+                    Id = 139,
+                    Name = "大橋 孝仁",
+                    HireDate = new DateTime(2004, 12, 1),
+                },
+            };
+            using (var writer = XmlWriter.Create(outfile)) {
+                var serializer = new DataContractSerializer(emps.GetType());
+                serializer.WriteObject(writer, emps);
+            }
+        }
+
+        private static void Exercise1_3(string file) {
+
+
+            //foreach (var emp in emps) {
+            //    Console.WriteLine("{0} {1} {2}", emp.Id, emp.Name, emp.HireDate);
+            //}
+        }
+
+        private static void Exercise1_4(string file) {
+
+        }
+    }
+}
