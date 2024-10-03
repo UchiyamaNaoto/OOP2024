ae8e241544afdb4d65e2f8b8af6448c4cfacaeca
UchiyamaNaoto <63093527+UchiyamaNaoto@users.noreply.github.com>
2024-10-03 11:40:58 +0900

    16章Section03終了

---
+++ Chapter16/Section03/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Section03 {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.bt_16_6 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.bt_16_7 = new System.Windows.Forms.Button();
+            this.bt_16_8 = new System.Windows.Forms.Button();
+            this.bt_16_9 = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // bt_16_6
+            // 
+            this.bt_16_6.Location = new System.Drawing.Point(28, 12);
+            this.bt_16_6.Name = "bt_16_6";
+            this.bt_16_6.Size = new System.Drawing.Size(266, 23);
+            this.bt_16_6.TabIndex = 0;
+            this.bt_16_6.Text = "イベントハンドラを非同期にする(1)";
+            this.bt_16_6.UseVisualStyleBackColor = true;
+            this.bt_16_6.Click += new System.EventHandler(this.bt_16_6_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 261);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(329, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // bt_16_7
+            // 
+            this.bt_16_7.Location = new System.Drawing.Point(28, 54);
+            this.bt_16_7.Name = "bt_16_7";
+            this.bt_16_7.Size = new System.Drawing.Size(266, 23);
+            this.bt_16_7.TabIndex = 2;
+            this.bt_16_7.Text = "イベントハンドラを非同期にする(2)";
+            this.bt_16_7.UseVisualStyleBackColor = true;
+            this.bt_16_7.Click += new System.EventHandler(this.bt_16_7_Click);
+            // 
+            // bt_16_8
+            // 
+            this.bt_16_8.Location = new System.Drawing.Point(28, 98);
+            this.bt_16_8.Name = "bt_16_8";
+            this.bt_16_8.Size = new System.Drawing.Size(266, 23);
+            this.bt_16_8.TabIndex = 2;
+            this.bt_16_8.Text = "非同期メソッドの定義（戻り値のない場合） ";
+            this.bt_16_8.UseVisualStyleBackColor = true;
+            this.bt_16_8.Click += new System.EventHandler(this.bt_16_8_Click);
+            // 
+            // bt_16_9
+            // 
+            this.bt_16_9.Location = new System.Drawing.Point(28, 143);
+            this.bt_16_9.Name = "bt_16_9";
+            this.bt_16_9.Size = new System.Drawing.Size(266, 23);
+            this.bt_16_9.TabIndex = 2;
+            this.bt_16_9.Text = "非同期メソッドの定義（戻り値のある場合） ";
+            this.bt_16_9.UseVisualStyleBackColor = true;
+            this.bt_16_9.Click += new System.EventHandler(this.bt_16_9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(329, 283);
+            this.Controls.Add(this.bt_16_9);
+            this.Controls.Add(this.bt_16_8);
+            this.Controls.Add(this.bt_16_7);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.bt_16_6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt_16_6;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Button bt_16_7;
+        private System.Windows.Forms.Button bt_16_8;
+        private System.Windows.Forms.Button bt_16_9;
+    }
+}
+
---
+++ Chapter16/Section03/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Section03 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        //非同期メソッド
+        private async void bt_16_6_Click(object sender, EventArgs e) {
+            toolStripStatusLabel1.Text = "";
+            await Task.Run(() => DoSomething());
+            //DoSomething();
+            toolStripStatusLabel1.Text = "終了";
+        }
+
+        private void DoSomething() {
+            Thread.Sleep(5000);
+        }
+
+        //非同期メソッド（イベントハンドラ）
+        private async void bt_16_7_Click(object sender, EventArgs e) {
+            toolStripStatusLabel1.Text = "";
+            var elapsed = await Task.Run(() => DoSomething2());
+            toolStripStatusLabel1.Text = $"{elapsed}ミリ秒";
+        }
+
+        private long DoSomething2() {
+            var sw = Stopwatch.StartNew();
+            Thread.Sleep(5000);
+            sw.Stop();
+            return sw.ElapsedMilliseconds;
+        }
+
+        private async void bt_16_8_Click(object sender, EventArgs e) {
+            toolStripStatusLabel1.Text = "";
+            await DoSomethingAsync();
+            toolStripStatusLabel1.Text = "終了";
+        }
+
+        //非同期メソッド
+        private async Task DoSomethingAsync() {
+            await Task.Run(() => {
+                Thread.Sleep(5000);
+            });
+        }
+
+        private async void bt_16_9_Click(object sender, EventArgs e) {
+            toolStripStatusLabel1.Text = "";
+            var elapsed = await DoSomethingAsync2();
+            toolStripStatusLabel1.Text = $"{elapsed}ミリ秒";
+        }
+        
+        //非同期メソッド
+        private async Task<long> DoSomethingAsync2() {
+            var sw = Stopwatch.StartNew();
+            await Task.Run(() => {
+                Thread.Sleep(5000);
+            });
+            sw.Stop();
+            return sw.ElapsedMilliseconds;
+        }
+    }
+}
