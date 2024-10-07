82fee46bbfab2dc8a4613a2f83cdf936ad3369ae
UchiyamaNaoto <63093527+UchiyamaNaoto@users.noreply.github.com>
2024-10-07 15:10:53 +0900

    16章まで完成

---
+++ Chapter16/Section04/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace Section04 {
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private async void bt_16_10_Click(object sender, RoutedEventArgs e) {
+            textBlock.Text = "";
+            var text = await GetPageAsync(@"http://www.bing.com/");
+            textBlock.Text = text;
+        }
+
+        private HttpClient _httpClient = new HttpClient();
+        
+        private async Task<string> GetPageAsync(string urlstr) {
+            var str = await _httpClient.GetStringAsync(urlstr);
+            return str;
+        }
+
+        private async void bt_16_11_Click(object sender, RoutedEventArgs e) {
+            textBlock.Text = "";
+            var text = await GetFromWikipediaAsync("UWP");
+            textBlock.Text = text;
+        }
+
+        private async Task<string> GetFromWikipediaAsync(string keyword) {
+            // UriBuilderとFormUrlEncodedContentを使い、パラメータ付きのURLを組み立てる
+            var builder = new UriBuilder("https://ja.wikipedia.org/w/api.php");
+            var content = new FormUrlEncodedContent(new Dictionary<string, string>() {
+                ["action"] = "query",
+                ["prop"] = "revisions",
+                ["rvprop"] = "content",
+                ["format"] = "xml",
+                ["titles"] = keyword,
+            });
+            builder.Query = await content.ReadAsStringAsync();
+
+            // HttpClientを使い、wikipediaのデータを取得する。
+            var str = await _httpClient.GetStringAsync(builder.Uri);
+
+            // 取得したXML文字列から、LINQ to XMLを使い必要な情報を取り出す。
+            var xmldoc = XDocument.Parse(str);
+            var rev = xmldoc.Root.Descendants("rev").FirstOrDefault();
+            return WebUtility.HtmlDecode(rev?.Value);
+        }
+
+        private async void bt_16_23_Click(object sender, RoutedEventArgs e) {
+            var tasks = new Task<string>[] {
+                  GetPageAsync(@"http://msdn.microsoft.com/magazine/"),
+                  GetPageAsync(@"http://msdn.microsoft.com/ja-jp/"),
+               };
+            var results = await Task.WhenAll(tasks);
+
+            // それぞれ先頭300文字を表示する
+            textBlock.Text =
+               results[0].Substring(0, 300) +
+               Environment.NewLine + Environment.NewLine +
+               results[1].Substring(0, 300);
+        }
+
+    }
+}
---
+++ Chapter16/Section06/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section06 {
+    internal class Program {
+        static async Task Main(string[] args) {
+            //var sw = Stopwatch.StartNew();
+            //var prime1 = GetPrimeAt5000();
+            //var prime2 = GetPrimeAt6000();
+            //sw.Stop();
+            //Console.WriteLine(prime1);
+            //Console.WriteLine(prime2);
+            //Console.WriteLine($"実行時間: {sw.ElapsedMilliseconds}ミリ秒");
+
+            var sw = Stopwatch.StartNew();
+            var task1 = Task.Run(() => GetPrimeAt5000());
+            var task2 = Task.Run(() => GetPrimeAt6000());
+            var prime1 = await task1;
+            var prime2 = await task2;
+            sw.Stop();
+            Console.WriteLine(prime1);
+            Console.WriteLine(prime2);
+            Console.WriteLine($"実行時間: {sw.ElapsedMilliseconds}ミリ秒");
+        }
+        // List 16-20
+
+        // 5000番目の素数を求める
+        private static int GetPrimeAt5000() {
+            return GetPrimes().Skip(4999).First();
+        }
+
+        // 6000番目の素数を求める
+        private static int GetPrimeAt6000() {
+            return GetPrimes().Skip(5999).First();
+        }
+
+        // 上記2つのメソッドから呼び出される下位メソッド
+        // あえて効率の悪いアルゴリズムで記述しています。
+        static IEnumerable<int> GetPrimes() {
+            for (int i = 2; i < int.MaxValue; i++) {
+                bool isPrime = true;
+                for (int j = 2; j < i; j++) {
+                    if (i % j == 0) {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                    yield return i;
+            }
+        }
+    }
+}
