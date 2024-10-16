c441b8a97a05d5c57a2e078485faaf8a5f7ac2f0
UchiyamaNaoto <63093527+UchiyamaNaoto@users.noreply.github.com>
2024-10-16 14:58:15 +0900

    問題17.3実装

---
+++ Chapter17/TemplateMethod/TextNumberSizeChange/Framework/TextFileProcessor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextNumberSizeChange.Framework {
+    public class TextFileProcessor {
+        private ITextFileService _service;
+
+        public TextFileProcessor(ITextFileService service) {
+            _service = service;
+        }
+
+        public void Run(string fileName) {
+            _service.Initialize(fileName);
+            using (var sr = new StreamReader(fileName)) {
+                while (!sr.EndOfStream) {
+                    string line = sr.ReadLine();
+                    _service.Execute(line);
+                }
+            }
+            _service.Terminate();
+        }
+    }
+}
---
+++ Chapter17/TemplateMethod/TextNumberSizeChange/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextFileProcessor;
+
+namespace TextNumberSizeChange {
+    internal class Program {
+        static void Main(string[] args) {
+            //TextProcessor.Run<ToHankakuProcessor>(args[0]);
+
+            var processor = new Framework.TextFileProcessor(new ToHankakuProcessor());
+            processor.Run(args[0]);
+        }
+    }
+}
