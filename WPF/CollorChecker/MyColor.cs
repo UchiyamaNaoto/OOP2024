6bcd0bdc59eed5acc6359f1ce0f2000a72859f08
UchiyamaNaoto <63093527+UchiyamaNaoto@users.noreply.github.com>
2024-10-23 13:30:48 +0900

    ColorCheckerのView作成とMyColorクラスの追加

---
+++ WPF/CollorChecker/MyColor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace CollorChecker {
+    public class MyColor {
+        public Color Color { get; set; }
+        public string Name { get; set; } = string.Empty;
+        //public override string ToString() {
+        //    return      
+        //}
+    }
+}
