1beb6ca1316eb72be1d5515a39cabb98863616a7
UchiyamaNaoto <63093527+UchiyamaNaoto@users.noreply.github.com>
2024-11-14 11:30:07 +0900

    住所追加　リストビューのスクロール

---
+++ SQLite/CustomerApp/Objects/Customer.cs
@@ -0,0 +1,30 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerApp.Objects {
+    public class Customer {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        /// <summary>
+        /// 名前
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 電話番号
+        /// </summary>
+        public string Phone { get; set; }
+
+        /// <summary>
+        /// 住所
+        /// </summary>
+        public string Address { get; set; }
+
+        public override string ToString() {
+            return $"{Id}  {Name}  {Phone}  {Address}";
+        }
+    }
+}
