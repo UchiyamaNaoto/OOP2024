ad1b0e01fab9281e160c567e7d15b1e769cd55c5
UchiyamaNaoto <63093527+UchiyamaNaoto@users.noreply.github.com>
2024-11-25 13:16:56 +0900

    char配列対応

--- SQLite/CustomerApp/MainWindow.xaml.cs
+++ SQLite/CustomerApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using CustomerApp.Objects;
+using Microsoft.Win32;
 using SQLite;
 using System;
 using System.Collections.Generic;
@@ -6,6 +7,7 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -24,23 +26,28 @@ namespace CustomerApp {
     /// </summary>
     public partial class MainWindow : Window {
         List<Customer> _customers;
+        string picPath = "";
         public MainWindow() {
             InitializeComponent();
         }
 
         private void SaveButton_Click(object sender, RoutedEventArgs e) {
-            Bitmap bmp = new Bitmap(@"C:\Users\infosys\Desktop\画像集\30ソアラ.jpg");
-            MemoryStream ms = new MemoryStream();
+            Customer customer = new Customer();
 
+            customer.Name = NameTextBox.Text;
+            customer.Phone = PhoneTextBox.Text;
+            customer.Address = AddressTextBox.Text;
+            customer.Picture = null;
 
-            var customer = new Customer() {
-                Name = NameTextBox.Text,
-                Phone = PhoneTextBox.Text,
-                Address = AddressTextBox.Text,
-                Picture = ms.ToArray(),
-            };
-            
-            using(var connection = new SQLiteConnection(App.databasePass)) {
+            if (picPath != "") {
+                Bitmap bmp = new Bitmap(picPath);
+                MemoryStream ms = new MemoryStream();
+                bmp.Save(ms, ImageFormat.Jpeg);
+                customer.Picture = ms.ToArray();
+                picPath = "";
+            }
+
+            using (var connection = new SQLiteConnection(App.databasePass)) {
                 connection.CreateTable<Customer>();
                 connection.Insert(customer);
             }
@@ -48,7 +55,7 @@ namespace CustomerApp {
         }
 
         private void UpdateButton_Click(object sender, RoutedEventArgs e) {
-           
+
 
 
         }
@@ -63,13 +70,13 @@ namespace CustomerApp {
         }
 
         private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e) {
-            var filterList = _customers.Where(x=>x.Name.Contains(SearchTextBox.Text)).ToList();
+            var filterList = _customers.Where(x => x.Name.Contains(SearchTextBox.Text)).ToList();
             CustomerListView.ItemsSource = filterList;
         }
 
         private void DeleteButton_Click(object sender, RoutedEventArgs e) {
             var item = CustomerListView.SelectedItem as Customer;
-            if(item == null) {
+            if (item == null) {
                 MessageBox.Show("削除する行を選択してください");
                 return;
             }
@@ -88,9 +95,33 @@ namespace CustomerApp {
         }
 
         private void CustomerListView_SelectionChanged(object sender, SelectionChangedEventArgs e) {
-            NameTextBox.Text = _customers[CustomerListView.SelectedIndex].Name;
+            if (CustomerListView.SelectedIndex != -1) {
+                NameTextBox.Text = _customers[CustomerListView.SelectedIndex].Name;
+                PhoneTextBox.Text = _customers[CustomerListView.SelectedIndex].Phone;
+                AddressTextBox.Text = _customers[CustomerListView.SelectedIndex].Address;
+                PictureImageBox.Source = Create(_customers[CustomerListView.SelectedIndex].Picture);
+            }
+        }
+        public static BitmapImage Create(byte[] bytes) {
+            var result = new BitmapImage();
 
+            using (var stream = new MemoryStream(bytes)) {
+                result.BeginInit();
+                result.CacheOption = BitmapCacheOption.OnLoad;
+                result.CreateOptions = BitmapCreateOptions.None;
+                result.StreamSource = stream;
+                result.EndInit();
+                result.Freeze();    // 非UIスレッドから作成する場合、Freezeしないとメモリリークするため注意
+            }
+            return result;
+        }
 
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() != null) {
+                picPath = ofd.FileName;
+                PictureImageBox.Source = new BitmapImage(new Uri(picPath, UriKind.Absolute));
+            }
         }
     }
 }
